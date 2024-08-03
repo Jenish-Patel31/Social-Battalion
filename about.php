e38d22ac4c198084b2014beed8e9fce65be2bb13
PATEL JENISH KANTILAL <23bce529@nirmauni.ac.in>
2024-08-03 15:16:14 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,273 @@
+<?php
+include("header.php");
+?>
+<div id="home-owl" class="owl-carousel owl-theme">
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/IMG_20191210_133502.jpg);"></div>
+
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+<h1>Social Battalion</h1>
+<p class="lead">Help India’s children find their true potential. Donate now!.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/IMG_20191210_124518.jpg);"></div>
+
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+<h1>Social Battalion</h1>
+<p class="lead">Fundraise or donate with JustGiving, the worlds leading online fundraising platform, helping charities to make more with GiftAid.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</header>
+
+
+<div id="about" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-5">
+<div class="section-title">
+<h2 class="title">Welcome to Social Battalion</h2>
+<p class="sub-title">Social Battalion is a non-governmental organization committed to making a positive impact on society through its various programs and initiatives. With the aim of uplifting underprivileged communities and individuals, Social Battalion works towards the welfare of the society in a holistic way.</p>
+</div>
+</div>
+
+
+<div class="col-md-offset-1 col-md-6">
+<a href="#" class="about-video">
+<img src="img/charity/IMG_20191210_133502.jpg" style="heighT: 500px;">
+</a>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="numbers" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-smile-o"></i>
+<h3>100</h3>
+<span>Donors</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-heartbeat"></i>
+<h3>100K</h3>
+<span>Children Saved</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-money"></i>
+<h3>240</h3>
+<span>Donated</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-handshake-o"></i>
+<h3>12</h3>
+<span>Volunteers</span>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<hr>
+<div id="causes" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-12">
+<div class="section-title">
+<b>Social Battalion</b>
+<p class="sub-title">Social Battalion is a non-governmental organization committed to making a positive impact on society through its various programs and initiatives. With the aim of uplifting underprivileged communities and individuals, Social Battalion works towards the welfare of the society in a holistic way.</p>
+<p class="sub-title">At Social Battalion, we believe that access to basic needs such as food, shelter, healthcare, and education is a fundamental right. We strive to create a society where everyone can live with dignity and have access to equal opportunities for growth and development.</p>
+<p class="sub-title">Our organization works on various social issues such as poverty, malnutrition, illiteracy, and health problems faced by marginalized communities. We believe in empowering individuals and communities to become self-sufficient and independent through our various programs.</p>
+<p class="sub-title">Our flagship programs include providing nutritious meals and shelter to the homeless, providing education to children from economically disadvantaged backgrounds, providing healthcare facilities to those in need, and empowering women by providing them with vocational training.</p>
+<p class="sub-title">We also conduct various community outreach programs that focus on creating awareness about important social issues and help in bringing about a positive change in the society.</p>
+<p class="sub-title">At Social Battalion, we believe in transparency and accountability in all our activities. We work towards ensuring that every rupee donated to us is utilized for the betterment of the society in the most efficient manner possible.We welcome individuals and organizations who share our vision and mission to join hands with us in our efforts towards creating a better and more equitable society. Together, we can make a difference and work towards a brighter future for all.</p><br>
+
+
+<b>SERVICE.</b>
+<p class="sub-title">Our residents belong to the poorest of the poor families without the economic, cultural or educational background of the privileged, without access even to the basic necessities of life like shelter, food, healthcare and education. Having lived a life of extreme poverty and hardships, they are depressed  without any enthusiasm in life. As such all our activities are centered on changing their attitude towards life and motivating them to dignified.</p>
+<p class="sub-title">living with awareness programme of social security and benefits to elders by the Government. Free eye camps, medical camps, dental camps are regularly conducted by Sahaya Charity at backward remote villages, in association with various social service organisations and through other Medical and Dental Colleges. Rehabilitation of widows, destitutes and  needy are given priority. Living quarters are kept clean with regular maintenance. Sahaya Charity with both units is  considered as the one of the best managed organisation, known for its personal care and medical facilities..</p>
+
+<p class="sub-title">We appeal to the privileged sections of the society
+                             to please extend their helping hand.
+</p>
+
+
+
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="cta" class="section">
+
+<div class="section-bg" style="background-image: url(img/background-1.jpg);" data-stellar-background-ratio="0.5"></div>
+
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-offset-2 col-md-8">
+<div class="cta-content text-center">
+<h1>Join with Us</h1>
+<p class="lead">Your support is important to our work. There are many ways you can contribute towards our causes, and every little bit that you commit goes a long way in helping us fulfill our mission. Learn more about how you can get involved and take advantage of the opportunity to do some good.
+..</p>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="events" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-8 col-md-offset-2">
+<div class="section-title text-center">
+<h2 class="title">Social Battalion</h2>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-1.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Help people in need</a></h3>
+<p>Provide direct support to an individual, family or community by paying medical expenses or offering financial aid..</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-2.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Take action in an emergency</a></h3>
+
+<p>Raise funds in response to a natural disaster or humanitarian crisis. Make a difference in minutes.</p>
+</div>
+</div>
+</div>
+
+<div class="clearfix visible-md visible-lg"></div>
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-3.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Take part in a charity event</a></h3>
+
+<p>Choose from hundreds of official events including marathons, bike rides, Dryathlons and bake offs….</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-4.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Celebrate an occasion</a></h3>
+<p>Mark a special event like a birthday, wedding or final exam by asking friends for donations rather than gifts..</p>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<?php
+include("footer.php");
+?>
---
+++ databaseconnection.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+// Create connection
+$con=mysqli_connect("localhost:3306","root","","u993820046_ngo");
+echo mysqli_connect_error();
+?>
